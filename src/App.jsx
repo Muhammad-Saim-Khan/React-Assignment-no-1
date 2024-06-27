fca71ab754745284982b0a1d8eb0002119dec74f
Muhammad-Saim-Khan <khanmsaim89@gmail.com>
2024-06-27 18:52:20 +0500

    This is First Assignment of React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Name from "./Name";
+const App = () => {
+  return (
+    <div>
+      <Name />
+      <div class="card">
+        <div class="one">
+          <h1>M.SAIM.KHAN</h1>
+          <a href="#" className="btn">
+            View Portfolio
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ src/Name.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+const Name = () => {
+  return (
+    <center>
+      <h1 className="Name">Muhammad Saim Khan</h1>
+    </center>
+  );
+};
+export default Name;
